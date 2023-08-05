560c9932602a1254515508db335e805169988b29
sami <s.a.sami359359@gmail.com>
2023-08-05 14:21:50 +0530

    removed apostrophes

--- app/about/page.js
+++ app/about/page.js
@@ -17,11 +17,11 @@ export default function About() {
       <h1 className='md:text-5xl text-4xl gradient-text text-center mt-10 mb-10' data-aos="fade-up">About Us</h1>
       <div className='flex flex-col justify-between lg:gap-5 lg:w-3/4 m-auto  lg:flex-row-reverse'>
         <Image src="/images/about pic 1.jpg" className='px-5' width={400} height={400} data-aos="fade-up" />
-        <p className='lg:w-1/2 px-5 mt-10 text-lg font-thin' data-aos="fade-up">At  <span style={{ color: "#FF4081" }} >Qidmat Kirana Store</span>, we take pride in providing top-quality, handpicked daily essentials to our valued customers. With a wide array of products ranging from nutritious pulses, fragrant rice, and wholesome flour to aromatic spices, nourishing cooking oils, and refreshing dairy products like milk and curd, we've got everything you need to fill your kitchen with goodness.</p>
+        <p className='lg:w-1/2 px-5 mt-10 text-lg font-thin' data-aos="fade-up">At  <span style={{ color: "#FF4081" }} >Qidmat Kirana Store</span>, we take pride in providing top-quality, handpicked daily essentials to our valued customers. With a wide array of products ranging from nutritious pulses, fragrant rice, and wholesome flour to aromatic spices, nourishing cooking oils, and refreshing dairy products like milk and curd, we have got everything you need to fill your kitchen with goodness.</p>
       </div>
       <div className='flex justify-between flex-col lg:gap-5 lg:w-3/4 mt-10 m-auto lg:flex-row'>
         <Image src="/images/about pic 2.png" data-aos="fade-up" width={400} height={400} />
-        <p className='lg:w-1/2 mt-10 text-lg px-5' data-aos="fade-up">At <span style={{ color: "#FF4081" }}>Qidmat Kirana Store</span>, we believe that convenience should be a part of your shopping experience. That's why we offer a hassle-free home delivery service on orders above 1000 rupees, right to your doorstep. No more lugging heavy grocery bags or waiting in long queues—just sit back, relax, and let us bring the goodness to you.</p>
+        <p className='lg:w-1/2 mt-10 text-lg px-5' data-aos="fade-up">At <span style={{ color: "#FF4081" }}>Qidmat Kirana Store</span>, we believe that convenience should be a part of your shopping experience. That is why we offer a hassle-free home delivery service on orders above 1000 rupees, right to your doorstep. No more lugging heavy grocery bags or waiting in long queues—just sit back, relax, and let us bring the goodness to you.</p>
       </div>
 
       <div>
